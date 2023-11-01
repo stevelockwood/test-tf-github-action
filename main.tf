11b1c38675eb7fcf3232806baebe657d6b22fbe0
stevelockwood <steve@lochnetsystems.com>
2023-11-01 11:51:38 +0000

    deleted spaces

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 resource "random_id" "random_name" {
   byte_length = 8
 }
@@ -7,8 +6,5 @@ resource "google_storage_bucket" "test-bucket" {
   name          = "test-bucket-${random_id.random_name.hex}"
   location      = "us-east1"
   storage_class = "STANDARD"
-
-
-
 }
 
